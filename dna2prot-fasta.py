293035640646585736f0c74a9833a1cba240a7c7
Ben A <ben@theaarons.com>
2017-02-07 15:02:08 -0500

    Added Codon Dict and rev_comp fxn

--- dna2prot-fasta.py
+++ dna2prot-fasta.py
@@ -1,3 +1,31 @@
+RNA_codon_table = {
+# 			Second Base
+#    U 	         C           A           G
+# U
+'UUU': 'F', 'UCU': 'S', 'UAU': 'Y', 'UGU': 'C', # UxU
+'UUC': 'F', 'UCC': 'S', 'UAC': 'Y', 'UGC': 'C', # UxC
+'UUA': 'L', 'UCA': 'S', 'UAA': '*', 'UGA': '*', # UxA
+'UUG': 'L', 'UCG': 'S', 'UAG': '*', 'UGG': 'W', # UxG
+# C
+'CUU': 'L', 'CCU': 'P', 'CAU': 'H', 'CGU': 'R', # CxU
+'CUC': 'L', 'CCC': 'P', 'CAC': 'H', 'CGC': 'R', # CxC
+'CUA': 'L', 'CCA': 'P', 'CAA': 'Q', 'CGA': 'R', # CxA
+'CUG': 'L', 'CCG': 'P', 'CAG': 'Q', 'CGG': 'R', # CxG
+# A
+'AUU': 'I', 'ACU': 'T', 'AAU': 'N', 'AGU': 'S', # AxU
+'AUC': 'I', 'ACC': 'T', 'AAC': 'N', 'AGC': 'S', # AxC
+'AUA': 'I', 'ACA': 'T', 'AAA': 'K', 'AGA': 'R', # AxA
+'AUG': 'M', 'ACG': 'T', 'AAG': 'K', 'AGG': 'R', # AxG
+# G
+'GUU': 'V', 'GCU': 'A', 'GAU': 'D', 'GGU': 'G', # GxU
+'GUC': 'V', 'GCC': 'A', 'GAC': 'D', 'GGC': 'G', # GxC
+'GUA': 'V', 'GCA': 'A', 'GAA': 'E', 'GGA': 'G', # GxA
+'GUG': 'V', 'GCG': 'A', 'GAG': 'E', 'GGG': 'G'  # GxG
+}
+
+def rev_complement(seq):
+    return seq[::-1]
+
 file_location = raw_input("Where is your FASTA file? ")
 stripped = []
 metadata = []
